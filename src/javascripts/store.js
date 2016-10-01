92a1ab7e370520c0b332a25c5f3a51679197e5ff
Skip McBride <skip@zerosquadron.com>
2016-09-30 19:00:29 -0700

    created store constructor and got 5 passing tests

--- src/javascripts/store.js
+++ src/javascripts/store.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  
+class Store {
+  getState (id) {
+    const square = Array(3).fill(0);
+    const gameArray = Array(3).fill(square);
+
+    if (id === 'gameboard') {
+      return gameArray;
+    }
+    if (id === 'message') {
+      return '';
+    }
+  }
+
+
 }
+
+
+
+module.exports = Store;
--- test/store_spec.js
+++ test/store_spec.js
@@ -5,4 +5,35 @@ describe('Store', () => {
   it('is a function', () => {
     expect(Store).to.be.a('function');
   });
+  it('return an object', () => {
+    const actual = new Store();
+    expect(actual).to.be.an('object');
+  })
+
+  describe('instance', () => {
+    let instance;
+    beforeEach(() => {
+      instance = new Store();
+    });
+
+    afterEach(() => {
+      instance = null;
+    });
+
+    describe('.getState', () => {
+      it('can return gameboard state', () => {
+        const actual = instance.getState('gameboard');
+        expect(actual).to.eql([
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0],
+        ]);
+      });
+
+      it('can return message', () => {
+        const actual = instance.getState('message');
+        expect(actual).to.eql('');
+      })
+    });
+  });
 });
