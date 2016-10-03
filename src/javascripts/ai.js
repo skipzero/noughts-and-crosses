e3d4934b300887423df668aa8931999d384dca10
Skip McBride <skip@zerosquadron.com>
2016-10-02 21:13:00 -0700

    updated ai and ai spec

--- src/javascripts/ai.js
+++ src/javascripts/ai.js
@@ -1,6 +1,9 @@
+const Store = require('./store');
+const board = require('./board');
 
 const ai = () => {
 
+  return {};
 }
 
 module.exports = ai;
--- test/ai_spec.js
+++ test/ai_spec.js
@@ -4,5 +4,10 @@ const ai = require('../src/javascripts/ai');
 describe('Our AI ', () => {
   it('is a function', () => {
     expect(ai).to.be.a('function');
-  })
-})
+  });
+
+  it('returns an object', () => {
+    const actual = ai();
+    expect(actual).to.be.an('object');
+  });
+});
