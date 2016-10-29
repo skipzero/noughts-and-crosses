4e803587dece9babb1062f00ad72b2cecd880df6
Skip McBride <skip@zerosquadron.com>
2016-10-28 19:00:38 -0700

    commented out currently unused

--- src/javascripts/board.jsx
+++ src/javascripts/board.jsx
@@ -47,91 +47,91 @@ class Board extends React.Component {
 
   }
 
-  checkWin (index, turn) {
-    const squares = this.state.squares;
-    const winArray = [
-      [0, 1, 2],
-      [0, 3, 6],
-      [0, 4, 8],
-      [1, 4, 7],
-      [2, 5, 8],
-      [2, 4, 6],
-      [3, 4, 5],
-      [6, 7, 8],
-    ];
+  // checkWin (index, turn) {
+  //   const squares = this.state.squares;
+  //   const winArray = [
+  //     [0, 1, 2],
+  //     [0, 3, 6],
+  //     [0, 4, 8],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     [2, 4, 6],
+  //     [3, 4, 5],
+  //     [6, 7, 8],
+  //   ];
+  //
+  //   let message = this.state.message;
+  //   let round = this.state.round;
+  //   let timer;
+  //
+  //   for (let i = 0; i < winArray.length - 1; i++) {
+  //     const currArr1 = winArray[i][0];
+  //     const currArr2 = winArray[i][1];
+  //     const currArr3 = winArray[i][2];
+  //
+  //     //  Check if any of our winning conditions are met...
+  //     if (squares[currArr1] === squares[currArr2] && squares[currArr1] === squares[currArr3] && squares[currArr1] !== '') {
+  //       message = `The ${turn}'s win!! Good job. Play again!`;
+  //       break;
+  //     }
+  //     else if (round === 9) {
+  //       // Tie game TODO: add verbage to declare tied...
+  //       console.info('Tie...')
+  //       message = 'Game Over!\n Tie game... No winner...';
+  //       break;
+  //     }
+  //
+  //     if (message !== '') {
+  //       this.setState({
+  //         message
+  //       })
+  //     }
+  //   }
+  // }
 
-    let message = this.state.message;
-    let round = this.state.round;
-    let timer;
+  // compTurn () {
+  //   const squares = this.state.squares;
+  //   const selected = this.state.selected;
+  //
+  //   let round = this.state.round;
+  //   let turn = this.state.turn;
+  //   let index = this.randNum.call(this);
+  //
+  //   squares[index] = turn;
+  //   selected.push(index);
+  //
+  //
+  //   this.checkWin(index, turn);
+  //   turn = this.nextTurn(turn);
+  //   round += 1;
+  //   this.setState({
+  //     squares,
+  //     turn,
+  //     round,
+  //     selected,
+  //   });
+  // }
 
-    for (let i = 0; i < winArray.length - 1; i++) {
-      const currArr1 = winArray[i][0];
-      const currArr2 = winArray[i][1];
-      const currArr3 = winArray[i][2];
+  // nextTurn (turn) {
+  //   return turn = turn === 'O' ? 'X' : 'O';
+  // }
 
-      //  Check if any of our winning conditions are met...
-      if (squares[currArr1] === squares[currArr2] && squares[currArr1] === squares[currArr3] && squares[currArr1] !== '') {
-        message = `The ${turn}'s win!! Good job. Play again!`;
-        break;
-      }
-      else if (round === 9) {
-        // Tie game TODO: add verbage to declare tied...
-        console.info('Tie...')
-        message = 'Game Over!\n Tie game... No winner...';
-        break;
-      }
-
-      if (message !== '') {
-        this.setState({
-          message
-        })
-      }
-    }
-  }
-
-  compTurn () {
-    const squares = this.state.squares;
-    const selected = this.state.selected;
-
-    let round = this.state.round;
-    let turn = this.state.turn;
-    let index = this.randNum.call(this);
-
-    squares[index] = turn;
-    selected.push(index);
-
-
-    this.checkWin(index, turn);
-    turn = this.nextTurn(turn);
-    round += 1;
-    this.setState({
-      squares,
-      turn,
-      round,
-      selected,
-    });
-  }
-
-  nextTurn (turn) {
-    return turn = turn === 'O' ? 'X' : 'O';
-  }
-
-  randNum () {
-    let newIndex = Math.floor(Math.random() * 9);
-    const selected = this.state.selected;
-    if (selected.length <= 9) {
-
-      selected.forEach((cur) => {
-        if (cur === newIndex) {
-          this.randNum();
-          return;
-        }
-      });
-      return newIndex;
-    } else {
-      return;
-    }
-  }
+  // randNum () {
+  //   let newIndex = Math.floor(Math.random() * 9);
+  //   const selected = this.state.selected;
+  //   if (selected.length <= 9) {
+  //
+  //     selected.forEach((cur) => {
+  //       if (cur === newIndex) {
+  //         this.randNum();
+  //         return;
+  //       }
+  //     });
+  //     return newIndex;
+  //   } else {
+  //     return;
+  //   }
+  // }
 }
 
 Board.propTypes = {
